f1f7a9590ad1517b5bf75870cca0da36c059005a
Álvaro Lima <123199876+santosalvaro@users.noreply.github.com>
2026-09-03 09:58:22 -0300

    Commit 10: Replacing conditional with polymorphism - part 3.

--- model.py
+++ model.py
@@ -1,3 +1,41 @@
+class Price:
+
+    def get_charge(self, days_rented: int) -> float:
+        pass
+
+    def get_frequent_renter_points(self, days_rented: int) -> int:
+        return 1
+
+
+class RegulaPrice(Price):
+
+    def get_charge(self, days_rented: int) -> float:
+        amount = 2.0
+        if days_rented > 2:
+            amount += (days_rented - 2) * 1.5
+        return amount
+
+
+class NewReleasePrice(Price):
+
+    def get_charge(self, days_rented: int) -> float:
+        return days_rented * 3.0
+
+    def get_frequent_renter_points(self, days_rented: int) -> int:
+        points = 1
+        if days_rented > 1:
+            points += 1
+        return points
+
+
+class ChildrenPrice(Price):
+
+    def get_charge(self, days_rented: int) -> float:
+        amount = 1.5
+        if days_rented > 3:
+            amount += (days_rented - 3) * 1.5
+        return amount
+        
 class Book:
 
     REGULAR: int = 0
@@ -60,23 +98,3 @@ class Client:
         result += f"Total: {total_amount}\n"
         result += f"Points: {frequent_renter_points}"
         return result
-
-class Price:
-
-    def get_charge(self, days_rented: int) -> float:
-        pass
-
-    def get_frequent_renter_points(self, days_rented: int) -> int:
-        pass
-
-
-class RegulaPrice(Price):
-    pass
-
-
-class NewReleasePrice(Price):
-    pass
-
-
-class ChildrenPrice(Price):
-    pass
